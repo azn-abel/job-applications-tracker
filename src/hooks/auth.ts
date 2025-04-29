675ae16c40f0127e2d3da47dacead2b68ea50380
Abel Lu <alu4@ncsu.edu>
2025-04-29 18:17:36 -0400

    moving things around

--- src/hooks/auth.ts
+++ src/hooks/auth.ts
@@ -52,9 +52,9 @@ export default function useAuth() {
     const response = await requestLogout()
     if (response.success) {
       setUser(null)
-      setIsAuthenticated(false)
       setArchiveApplications([])
       setHomeApplications([])
+      setIsAuthenticated(false)
     }
   }
 
